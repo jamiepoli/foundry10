0f1cbf03636128fd6a792adad99cf57bea12a453
Jamie Polintan <jamiecpol@gmail.com>
2018-03-26 20:32:12 -0700

    Adding files

---
+++ AmericanHistorySimulatorBeta/Assets/SCRIPTS/Game_Specific/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    public float levelStartDelay = 2f;
+    
+
+    
+	// Use this for initialization
+	void Awake () {
+
+
+		if (instance == null)
+        {
+            instance = this;
+        }
+
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        SceneManager.LoadScene("Office");
+    }
+
+
+
+
+}
